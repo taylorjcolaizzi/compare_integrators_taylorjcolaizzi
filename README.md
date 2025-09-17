1e7de7ffb6476f5ce497696c098ed2ab540b45ea
Taylor Colaizzi <zxk4gs@virginia.edu>
2025-09-17 13:31:08 -0400

    now I've plotted both Errors and BadErrors

--- README.md
+++ README.md
@@ -17,3 +17,27 @@ You can also use the C++ example as a guide to build your own executable
 
 There is no need to look at rules >~25 for Gaussian quadrature.  And you can also stop at ~ 1000 divisions for the trapezoidal and Simpson's rules.  If you run much longer you'll see the numerical errors bevome visible for the trapezoidal, but you'll need to think about how to code efficiently or the running time may be very long.
 
+Question 1
+
+You can just look at my code named integrators.py. There, I've defined several functions that take care of this. However, you can clearly see that I haven't got a Gaussian quadrature function. That is because I ran out of time.
+
+Question 2
+
+I have a print out that runs when you type into the console the following command:
+
+python integrators.py
+
+Question 3
+
+You can look at my plot Errors.png to see how I've graphed the log-log of relative error versus N.
+
+Question 4
+
+To get the slopes, I just did elementary slope formula.
+
+For the algorithmic region, the slopes are
+trapezoid  = -1.01
+simpson = -3.89
+gauss = 0 since I don't have any function here.
+
+And in the round-off region,
--- integrators.py
+++ integrators.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import exp
+from math import sin
+from math import log10
+from math import cos
+from gqconstants import HighPrecisionGaussInt
 
 # trapezoid rule ~~~~~~~~~~~~~~~~~~~~~~~~
 def trapezoid(function, points, minimum, maximum, verbose = 0):
@@ -38,13 +42,17 @@ def simpson(function, points, minimum, maximum):
         print("Simpson's rule needs odd number of points to work correctly")
         return 1
     else:
+        points = int(points / 2 + .5)
         sum = 0.
         width = maximum - minimum
         h = width / points
         hthird = h / 3
         for i in range(points):
             x = minimum + i * h; # where we evaluate function
-            sum += hthird * ( function(x) + 4 * function(x + h/2) + function(x + h) )
+            # sum += hthird * ( function(x) + 2 * function(x + h) * (i%2) + 2 * function(x + h) + function(x + h + h) )
+            sum += hthird * ( function(x) + 4 * function(x + h/2) + function(x + h) ) / 2
+
+            # Originally I was double counting. Not sure how best to get rid of double counting without just dividing 2????
         return sum
             
 
@@ -53,7 +61,9 @@ def simpson(function, points, minimum, maximum):
 def gauss(function, points, minimum, maximum):
     """
     I got no idea how to do this currently. will come back and fix...
-    currently, it just does nothing.
+    currently, it just does nothing but return 0.
+
+    Maybe will implement with the gaussian quadrature in the gqconstants, but I don't know how that works yet...
     """
     return 0
 
@@ -64,12 +74,26 @@ def neg_exp(x):
     """
     return exp(-1 * x)
 
-def integral():
+def fast_sin(x):
+    """
+    takes in point. multiplies by 100. calculates sin
+    """
+    # return sin(100 * x)
+    return sin(100 * x)
+
+def integral_exp():
     """
     integral of e^(-x) from 0 to 1 is
     1 - e^(-1)
     """
     return 1 - exp(-1)
+
+def integral_sin():
+    """
+    integral of sin(100 * x) from 0 to 1 is
+    -.01cos(100 * x)
+    """
+    return (-0.01 * cos(100 * 1)) - (-0.01 * cos(100 * 0))
 
 def error(calculated, actual, absolute = 1):
     """
@@ -85,16 +109,74 @@ def error(calculated, actual, absolute = 1):
 
 # calculating the error ~~~~~~~~~~~~~~~~~
 
-spacings = [3, 11, 21, 41, 81, 161]
+spacings = [3, 11, 21, 41, 81, 161, 251, 581, 999, 2001, 5003, 100005]
+integrals = [integral_exp, integral_sin]
 minimum = 0.
 maximum = 1.
 # printing it out
 print("here is part 2 of the assignment for the following N values")
+print("---------------")
 print("N, e_T, e_S, e_G")
+print("---------------")
 for n in spacings:
     print(
         n,
-        error(trapezoid(neg_exp, n, minimum, maximum), integral()),
-        error(simpson(neg_exp, n, minimum, maximum), integral()),
-        error(gauss(neg_exp, n, minimum, maximum), integral())
+        error(trapezoid(neg_exp, n, minimum, maximum), integral_exp()),
+        error(simpson(neg_exp, n, minimum, maximum), integral_exp()),
+        error(gauss(neg_exp, n, minimum, maximum), integral_exp())
     )
+
+# making log log plot of relative error versus N ~~~~~~~
+methods = [trapezoid, simpson, gauss]
+functions = [neg_exp]
+# so here's the plotting. the method loop is from copilot with some name tweaks from me
+print('plotting and saving problem 3')
+print("-------------------")
+fig, axes = plt.subplots(1, 1, figsize = (10, 8))
+plt.suptitle("Plotting log-log Relative Error versus N")
+for i, func in enumerate(functions):
+    for method in methods:
+        now_error = [error(method(func, n, minimum, maximum), integral_exp()) for n in spacings]
+        axes.plot((spacings), (now_error), label = method.__name__, marker = 'o')
+        axes.set_ylabel("log of abs(error)")
+        axes.set_xlabel("log of N")
+        axes.set_title("integrate exp(-x)")
+        # axes.set_title("integrate sin(100x)")
+        axes.legend()
+        axes.set_xscale('log')
+        axes.set_yscale('log')
+        axes.grid()
+
+        print(method.__name__, "yields slope", (log10(now_error[6]) - log10(now_error[1])) / (log10(spacings[6]) - log10(spacings[1])))
+
+plt.tight_layout()
+plt.savefig("Errors.png")
+# print('plot done bruh')
+plt.show()
+
+
+# doing a tricky function sin(100x)
+
+# this will be tricky because you have fast oscillations. Need lots of steps here to work well.
+
+print('plotting for bad function sin(100x)')
+functions = [fast_sin]
+fig, axes = plt.subplots(1, 1, figsize = (10, 8))
+plt.suptitle("Plotting log-log Relative Error versus N")
+for i, func in enumerate(functions):
+    for method in methods:
+        now_error = [error(method(func, n, minimum, maximum), integral_sin()) for n in spacings]
+        axes.plot((spacings), (now_error), label = method.__name__, marker = 'o')
+        axes.set_ylabel("log of abs(error)")
+        axes.set_xlabel("log of N")
+        # axes.set_title("integrate exp(-x)")
+        axes.set_title("integrate sin(100x)")
+        axes.legend()
+        axes.set_xscale('log')
+        axes.set_yscale('log')
+        axes.grid()
+
+plt.tight_layout()
+plt.savefig("BadErrors.png")
+# print('plot done bruh')
+plt.show()
